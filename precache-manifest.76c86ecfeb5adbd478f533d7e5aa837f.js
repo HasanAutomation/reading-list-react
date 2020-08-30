5afbc7462dfda4b7fe77de6cc1fa57ba2f3e8379
Hasan Ali <kk631352@gmail.com>
2020-08-30 13:09:54 +0530

    Updates

---
+++ precache-manifest.76c86ecfeb5adbd478f533d7e5aa837f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcb45fa94e5f13611ef68fa7b877d68f",
+    "url": "/hasanautomation.github.io/reading-list-react/index.html"
+  },
+  {
+    "revision": "6cb534678450949184ce",
+    "url": "/hasanautomation.github.io/reading-list-react/static/css/main.d5b773ab.chunk.css"
+  },
+  {
+    "revision": "82aacac3f7fe56eb35f3",
+    "url": "/hasanautomation.github.io/reading-list-react/static/js/2.44960ffa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hasanautomation.github.io/reading-list-react/static/js/2.44960ffa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cb534678450949184ce",
+    "url": "/hasanautomation.github.io/reading-list-react/static/js/main.cdbc3893.chunk.js"
+  },
+  {
+    "revision": "febd730a9e00b46ade7a",
+    "url": "/hasanautomation.github.io/reading-list-react/static/js/runtime-main.ad649ef6.js"
+  }
+]);
